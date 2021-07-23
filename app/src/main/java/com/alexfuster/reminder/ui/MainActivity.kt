1082e58aefd74122862fce2bdfef4d9e87ea6969
Alejandro <confuste@gmail.com>
2021-07-23 13:30:54 +0200

    Add Navigation

---
+++ app/src/main/java/com/alexfuster/reminder/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.alexfuster.reminder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.alexfuster.reminder.R
+import com.alexfuster.reminder.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var navController: NavController
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    navController =  Navigation.findNavController(this, R.id.nav_host_fragment)
+    //NavigationUI.setupActionBarWithNavController(this, navController)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    navController.navigateUp()
+    return super.onSupportNavigateUp()
+
+  }
+}
